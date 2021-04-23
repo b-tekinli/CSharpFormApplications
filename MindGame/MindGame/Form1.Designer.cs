8fe3128d6774b358d2b4364a83904385f37e7c8a
b-tekinli <beyza.tekinli.bt@gmail.com>
2021-04-23 22:04:01 +0300

    added

---
+++ MindGame/MindGame/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace MindGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(169, 93);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(80, 27);
+            this.txtBox1.TabIndex = 0;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(64, 171);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(300, 27);
+            this.txtBox2.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Salmon;
+            this.btnStart.Location = new System.Drawing.Point(114, 228);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(201, 50);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Kontrol Et";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 341);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(418, 328);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Name = "Form1";
+            this.Text = "Mind Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MindGame/MindGame/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MindGame
+{
+    public partial class Form1 : Form
+    {
+        public string num1;
+        public string num2;
+        public string num3;
+        public string num4;
+        public string num5;
+        public string num6;
+        public string num7;
+        public string num8;
+        public string num9;
+        public string num10;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            randomNumber();
+
+            txtBox2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void randomNumber()
+        {
+            Random random = new Random();
+
+            int b = 0;
+
+            string num = "";
+
+            for (int a = 1; a <= 10; a++)
+            {
+                b = random.Next(0, 10);
+
+                num = num + b.ToString();
+
+                if (a == 1) { num1 = num; }
+                if (a == 2) { num2 = num; }
+                if (a == 3) { num3 = num; }
+                if (a == 4) { num4 = num; }
+                if (a == 5) { num5 = num; }
+                if (a == 6) { num6 = num; }
+                if (a == 7) { num7 = num; }
+                if (a == 8) { num8 = num; }
+                if (a == 9) { num9 = num; }
+                if (a == 10) { num10 = num; }
+            }
+
+            label1.Text = num;
+            txtBox2.Text = label1.Text.Substring(0, 1);
+            label1.Visible = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+
+            if (txtBox1.Text == num1) { flag = 1; txtBox2.Text = num2.Substring(1, 1); }
+            if (txtBox1.Text == num2) { flag = 1; txtBox2.Text = num3.Substring(2, 1); }
+            if (txtBox1.Text == num3) { flag = 1; txtBox2.Text = num4.Substring(3, 1); }
+            if (txtBox1.Text == num4) { flag = 1; txtBox2.Text = num5.Substring(4, 1); }
+            if (txtBox1.Text == num5) { flag = 1; txtBox2.Text = num6.Substring(5, 1); }
+            if (txtBox1.Text == num6) { flag = 1; txtBox2.Text = num7.Substring(6, 1); }
+            if (txtBox1.Text == num7) { flag = 1; txtBox2.Text = num8.Substring(7, 1); }
+            if (txtBox1.Text == num8) { flag = 1; txtBox2.Text = num9.Substring(8, 1); }
+            if (txtBox1.Text == num9) { flag = 1; txtBox2.Text = num10.Substring(9, 1); }
+            if (txtBox1.Text == num10) { flag = 1; txtBox2.Text = ("Tebrikler hafızanız çok güçlü :)"); }
+
+            txtBox1.Text = "";
+
+            if (flag == 0) { Application.Exit(); }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            txtBox1.Text = "";
+            timer1.Enabled = false;
+        }
+    }
+}
