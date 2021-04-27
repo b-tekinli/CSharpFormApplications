84e69883e2e3ed8f6b100838f1e645f25245ed9c
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-04-27 18:53:57 +0300

    added

---
+++ PrintNumbersSideBySide/PrintNumbersSideBySide/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace PrintNumbersSideBySide
+{
+    partial class Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxNum1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxNum2 = new System.Windows.Forms.TextBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxShow = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxNum3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBoxNum1
+            // 
+            this.txtBoxNum1.Location = new System.Drawing.Point(256, 40);
+            this.txtBoxNum1.Name = "txtBoxNum1";
+            this.txtBoxNum1.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxNum1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(37, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Başlangıç değerini giriniz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(72, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bitiş değerini giriniz: ";
+            // 
+            // txtBoxNum2
+            // 
+            this.txtBoxNum2.Location = new System.Drawing.Point(256, 89);
+            this.txtBoxNum2.Name = "txtBoxNum2";
+            this.txtBoxNum2.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxNum2.TabIndex = 2;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.BackColor = System.Drawing.Color.Transparent;
+            this.btnPrint.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPrint.ForeColor = System.Drawing.Color.Lime;
+            this.btnPrint.Location = new System.Drawing.Point(456, 54);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(100, 59);
+            this.btnPrint.TabIndex = 5;
+            this.btnPrint.Text = "YAZDIR";
+            this.btnPrint.UseVisualStyleBackColor = false;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(37, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sayılar:";
+            // 
+            // txtBoxShow
+            // 
+            this.txtBoxShow.Location = new System.Drawing.Point(37, 241);
+            this.txtBoxShow.Name = "txtBoxShow";
+            this.txtBoxShow.Size = new System.Drawing.Size(851, 27);
+            this.txtBoxShow.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(74, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Artış değerini giriniz:";
+            // 
+            // txtBoxNum3
+            // 
+            this.txtBoxNum3.Location = new System.Drawing.Point(258, 142);
+            this.txtBoxNum3.Name = "txtBoxNum3";
+            this.txtBoxNum3.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxNum3.TabIndex = 9;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(898, 280);
+            this.Controls.Add(this.txtBoxNum3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBoxShow);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxNum2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxNum1);
+            this.Name = "Form";
+            this.Text = "Sayıları Yan Yana Yazdırma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxNum1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxNum2;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxShow;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxNum3;
+    }
+}
+
---
+++ PrintNumbersSideBySide/PrintNumbersSideBySide/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintNumbersSideBySide
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(txtBoxNum1.Text);    // int tipinde num1 değişkeni tanımlayıp veriyi txtBoxNum1'e text olarak çevirdik.
+            int num2 = Convert.ToInt32(txtBoxNum2.Text);
+            int num3 = Convert.ToInt32(txtBoxNum3.Text);
+
+            for (int i = num1; i <= num2; i+=num3)          // döngü ile tanımlanan değerden istenen değere kadar sayıları 1 artırmayı sağladık.
+            {
+                if (i == num2)                              // şartımızı belirttik.
+                {
+                    txtBoxShow.Text += i.ToString();        // textBox'ın içine döngü içinde tanımladığımız değişkenin metodu olarak stringe çevirip yazdırdık.
+                }
+                else
+                {
+                    txtBoxShow.Text += i.ToString() + ", "; // sayılar arasına virgül koyarak yazdırdık.
+                }
+            }
+        }
+    }
+}
