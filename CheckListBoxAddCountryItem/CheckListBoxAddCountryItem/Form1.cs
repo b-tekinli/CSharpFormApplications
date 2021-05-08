4f5a85e80220853ffc22b3859e1efbce7018d512
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-05-08 05:49:31 +0300

    updated.

--- CheckListBoxAddCountryItem/CheckListBoxAddCountryItem/Form1.cs
+++ CheckListBoxAddCountryItem/CheckListBoxAddCountryItem/Form1.cs
@@ -17,28 +17,28 @@ namespace CheckListBoxAddCountryItem
             InitializeComponent();
         }
 
-        private void chkedLstBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        private void chkedLstBox_ItemCheck(object sender, ItemCheckEventArgs e)        // checkedListBox'ın item check özelliği çalıştığında
         {
             int selectedItem = e.Index;
-            string selectedText = chkedLstBox.Items[selectedItem].ToString();
+            string selectedText = chkedLstBox.Items[selectedItem].ToString();          // selectedText seçimi içinde checkedListBox'ın verilerini strin olarak tuttuk.
 
-            if (lstBoxAdd.Items.IndexOf(selectedItem) > -1)
+            if (lstBoxAdd.Items.IndexOf(selectedItem) > -1)                            // if ile listBox verilerinin index numarasını seçili olan verinin -1'den büyük olma şartını belirttik.
             {
                 return;
             }
 
-            bool isSelectedItem = chkedLstBox.GetItemChecked(selectedItem);
+            bool isSelectedItem = chkedLstBox.GetItemChecked(selectedItem);             // verilerin seçili olup olmadığını kontrol ettik.
 
-            if (isSelectedItem == false)
+            if (isSelectedItem == false)                                                // seçili olma durumunun yanlış olması halinde çalışacak şartı belirttik.
             {
-                lstBoxAdd.Items.Add(selectedText);
+                lstBoxAdd.Items.Add(selectedText);                                      // seçilen itemleri listBox'a ekledik.
             }
         }
 
 
-        private void btnAdd_Click(object sender, EventArgs e)
+        private void btnAdd_Click(object sender, EventArgs e)                           // ekleme butonunun çalışması halinde
         {
-            int add = chkedLstBox.Items.Count;
+            int add = chkedLstBox.Items.Count;                                          // checkedListBox'taki itemleri saydırdık.
 
             for (int i = 0; i < add; i++)
             {
@@ -47,9 +47,9 @@ namespace CheckListBoxAddCountryItem
         }
 
 
-        private void btnDell_Click(object sender, EventArgs e)
+        private void btnDell_Click(object sender, EventArgs e)                          // verileri kaldırma butonunun çalışması halinde
         {
-            lstBoxAdd.Items.Clear();
+            lstBoxAdd.Items.Clear();                                                    // listBox verilerini sildirdik.
         }
     }
 }
