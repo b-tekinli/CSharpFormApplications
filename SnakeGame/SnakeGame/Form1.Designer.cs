15a27928ceaf83339e190d4e34ac9487cc25a723
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-06-15 02:59:10 +0300

    added.

---
+++ SnakeGame/SnakeGame/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblGameTitle = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(12, 85);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(664, 580);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblGameTitle
+            // 
+            this.lblGameTitle.AutoSize = true;
+            this.lblGameTitle.Font = new System.Drawing.Font("Courier New", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblGameTitle.ForeColor = System.Drawing.Color.Red;
+            this.lblGameTitle.Location = new System.Drawing.Point(421, 22);
+            this.lblGameTitle.Name = "lblGameTitle";
+            this.lblGameTitle.Size = new System.Drawing.Size(217, 37);
+            this.lblGameTitle.TabIndex = 3;
+            this.lblGameTitle.Text = "SNAKE GAME";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 687);
+            this.Controls.Add(this.lblGameTitle);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblGameTitle;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SnakeGame/SnakeGame/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Snake snake = new Snake();
+        Direction direction1;
+        PictureBox[] pbSnakeParts;
+        bool anyFood = false;
+        Random random = new Random();
+        PictureBox pbFood;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            direction1 = new Direction(0, 0);
+            pbSnakeParts = new PictureBox[0];
+
+            for (int i = 0; i < 3; i++)                                 // yılanın parçalarının uzunluğu 3 olduğu için.
+            {
+                Array.Resize(ref pbSnakeParts, pbSnakeParts.Length + 1);
+                pbSnakeParts[i] = pbAdd();
+            }
+            timer1.Start();
+        }
+
+        private PictureBox pbAdd()
+        {
+            PictureBox pictureBox = new PictureBox();
+
+            pictureBox.Size = new Size(10, 10);
+            pictureBox.BackColor = Color.White;
+            pictureBox.Location = snake.GetPos(pbSnakeParts.Length - 1);
+            panel1.Controls.Add(pictureBox);
+            return pictureBox;
+        }
+
+        private void pbUpdate()
+        {
+            for (int i = 0; i < pbSnakeParts.Length; i++)
+            {
+                pbSnakeParts[i].Location = snake.GetPos(i);
+            }
+        }
+
+        // Yön tuşları ayarlandı.
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)       // yukarı ok tuşu ve W tuşuna basıldığında yukarı hareket etme ayarlandı.
+            {
+                if (direction1._y != 10)                          //yılanın kendiliğinden terse dönme durumu kontrol edildi.
+                {
+                    direction1 = new Direction(0, -10);
+                }
+                direction1 = new Direction(0, -10);
+            }
+
+            else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                if (direction1._y != -10)
+                {
+                    direction1 = new Direction(0, -10);
+                }
+                direction1 = new Direction(0, 10);
+            }
+
+            else if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                if (direction1._x != 10)
+                {
+                    direction1 = new Direction(0, -10);
+                }
+                direction1 = new Direction(-10, 0);
+            }
+
+            else if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                if (direction1._x != -10)
+                {
+                    direction1 = new Direction(0, -10);
+                }
+                direction1 = new Direction(10, 0);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            snake.Go(direction1);
+            pbUpdate();
+        }
+
+        public void createFood()
+        {
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.BackColor = Color.Red;
+            pictureBox.Size = new Size(10, 10);
+            pictureBox.Location = new Point(random.Next(panel1.Width / 10) * 10, random.Next(panel1.Height / 10) * 10);
+        }
+    }
+}
