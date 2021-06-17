81b39d0af1fe96bac5b241d81ae8c825b0776b8b
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-06-17 04:59:40 +0300

    updated.

--- ColorTeachingGame/ColorTeachingGame/Form1.Designer.cs
+++ ColorTeachingGame/ColorTeachingGame/Form1.Designer.cs
@@ -30,59 +30,234 @@ namespace ColorTeachingGame
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lblScoreCounter = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.pBarScore = new System.Windows.Forms.ProgressBar();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.btnPurple = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnBlack = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnOrange = new System.Windows.Forms.Button();
+            this.btnWhite = new System.Windows.Forms.Button();
+            this.btnPink = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // label1
+            // lblScoreCounter
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(67, 98);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblScoreCounter.AutoSize = true;
+            this.lblScoreCounter.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblScoreCounter.Location = new System.Drawing.Point(64, 5);
+            this.lblScoreCounter.Name = "lblScoreCounter";
+            this.lblScoreCounter.Size = new System.Drawing.Size(21, 24);
+            this.lblScoreCounter.TabIndex = 7;
+            this.lblScoreCounter.Text = "0";
             // 
-            // btnStart
+            // lblScore
             // 
-            this.btnStart.Location = new System.Drawing.Point(12, 448);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(94, 47);
-            this.btnStart.TabIndex = 1;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblScore.Location = new System.Drawing.Point(3, 5);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(55, 24);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.Text = "Skor:";
             // 
-            // panel1
+            // lblCounter
             // 
-            this.panel1.Location = new System.Drawing.Point(51, 265);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(250, 125);
-            this.panel1.TabIndex = 2;
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.BackColor = System.Drawing.Color.Transparent;
+            this.lblCounter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCounter.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCounter.Location = new System.Drawing.Point(306, 5);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(42, 26);
+            this.lblCounter.TabIndex = 8;
+            this.lblCounter.Text = "100";
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRed.Location = new System.Drawing.Point(3, 216);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(124, 67);
+            this.btnRed.TabIndex = 9;
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // pBarScore
+            // 
+            this.pBarScore.Location = new System.Drawing.Point(3, 55);
+            this.pBarScore.Name = "pBarScore";
+            this.pBarScore.Size = new System.Drawing.Size(644, 29);
+            this.pBarScore.TabIndex = 10;
+            this.pBarScore.Value = 100;
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblColor.Location = new System.Drawing.Point(291, 102);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(81, 39);
+            this.lblColor.TabIndex = 11;
+            this.lblColor.Text = "Renk";
+            // 
+            // btnPurple
+            // 
+            this.btnPurple.BackColor = System.Drawing.Color.DarkOrchid;
+            this.btnPurple.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPurple.Location = new System.Drawing.Point(393, 216);
+            this.btnPurple.Name = "btnPurple";
+            this.btnPurple.Size = new System.Drawing.Size(124, 67);
+            this.btnPurple.TabIndex = 12;
+            this.btnPurple.UseVisualStyleBackColor = false;
+            this.btnPurple.Click += new System.EventHandler(this.btnPurple_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.BackColor = System.Drawing.Color.DarkGray;
+            this.btnGray.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGray.Location = new System.Drawing.Point(523, 216);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(124, 67);
+            this.btnGray.TabIndex = 13;
+            this.btnGray.UseVisualStyleBackColor = false;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.Lime;
+            this.btnGreen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGreen.Location = new System.Drawing.Point(263, 216);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(124, 67);
+            this.btnGreen.TabIndex = 14;
+            this.btnGreen.UseVisualStyleBackColor = false;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnYellow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnYellow.Location = new System.Drawing.Point(3, 305);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(124, 67);
+            this.btnYellow.TabIndex = 15;
+            this.btnYellow.UseVisualStyleBackColor = false;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnBlack
+            // 
+            this.btnBlack.BackColor = System.Drawing.Color.Black;
+            this.btnBlack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBlack.Location = new System.Drawing.Point(133, 305);
+            this.btnBlack.Name = "btnBlack";
+            this.btnBlack.Size = new System.Drawing.Size(124, 67);
+            this.btnBlack.TabIndex = 16;
+            this.btnBlack.UseVisualStyleBackColor = false;
+            this.btnBlack.Click += new System.EventHandler(this.btnBlack_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBlue.Location = new System.Drawing.Point(523, 305);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(124, 67);
+            this.btnBlue.TabIndex = 17;
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnOrange
+            // 
+            this.btnOrange.BackColor = System.Drawing.Color.Orange;
+            this.btnOrange.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrange.Location = new System.Drawing.Point(263, 305);
+            this.btnOrange.Name = "btnOrange";
+            this.btnOrange.Size = new System.Drawing.Size(124, 67);
+            this.btnOrange.TabIndex = 18;
+            this.btnOrange.UseVisualStyleBackColor = false;
+            this.btnOrange.Click += new System.EventHandler(this.btnOrange_Click);
+            // 
+            // btnWhite
+            // 
+            this.btnWhite.BackColor = System.Drawing.Color.White;
+            this.btnWhite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWhite.Location = new System.Drawing.Point(133, 216);
+            this.btnWhite.Name = "btnWhite";
+            this.btnWhite.Size = new System.Drawing.Size(124, 67);
+            this.btnWhite.TabIndex = 19;
+            this.btnWhite.UseVisualStyleBackColor = false;
+            this.btnWhite.Click += new System.EventHandler(this.btnWhite_Click);
+            // 
+            // btnPink
+            // 
+            this.btnPink.BackColor = System.Drawing.Color.Pink;
+            this.btnPink.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPink.Location = new System.Drawing.Point(393, 305);
+            this.btnPink.Name = "btnPink";
+            this.btnPink.Size = new System.Drawing.Size(124, 67);
+            this.btnPink.TabIndex = 20;
+            this.btnPink.UseVisualStyleBackColor = false;
+            this.btnPink.Click += new System.EventHandler(this.btnPink_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleTurquoise;
-            this.ClientSize = new System.Drawing.Size(659, 507);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(652, 387);
+            this.Controls.Add(this.btnPink);
+            this.Controls.Add(this.btnWhite);
+            this.Controls.Add(this.btnOrange);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnBlack);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.btnPurple);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.pBarScore);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.lblScoreCounter);
+            this.Controls.Add(this.lblScore);
             this.Name = "Form1";
-            this.Text = "Color  Teaching Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Label lblScoreCounter;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.ProgressBar pBarScore;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Button btnPurple;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnBlack;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnOrange;
+        private System.Windows.Forms.Button btnWhite;
+        private System.Windows.Forms.Button btnPink;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- ColorTeachingGame/ColorTeachingGame/Form1.cs
+++ ColorTeachingGame/ColorTeachingGame/Form1.cs
@@ -17,5 +17,207 @@ namespace ColorTeachingGame
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SelectColor();
+        }
+
+        public void SelectColor()
+        {
+            Random random = new Random();
+            int number = random.Next(0, 5);
+
+            if (number == 0)
+            {
+                lblColor.Text = "RED";
+            }
+            else if (number == 1)
+            {
+                lblColor.Text = "WHITE";
+            }
+            else if (number == 2)
+            {
+                lblColor.Text = "GREEN";
+            }
+            else if (number == 3)
+            {
+                lblColor.Text = "PURPLE";
+            }
+            else if (number == 4)
+            {
+                lblColor.Text = "GRAY";
+            }
+            else if (number == 5)
+            {
+                lblColor.Text = "YELLOW";
+            }
+            else if (number == 6)
+            {
+                lblColor.Text = "BLACK";
+            }
+            else if (number == 7)
+            {
+                lblColor.Text = "ORANGE";
+            }
+            else if (number == 8)
+            {
+                lblColor.Text = "PINK";
+            }
+            else if (number == 9)
+            {
+                lblColor.Text = "BLUE";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pBarScore.Value--;
+            lblCounter.Text = pBarScore.Value.ToString();
+
+            if (lblCounter.Text == "0")
+            {
+                timer1.Enabled = false;
+                btnRed.Enabled = false;
+                btnYellow.Enabled = false;
+                btnWhite.Enabled = false;
+                btnBlack.Enabled = false;
+                btnGreen.Enabled = false;
+                btnOrange.Enabled = false;
+                btnPurple.Enabled = false;
+                btnPink.Enabled = false;
+                btnGray.Enabled = false;
+                btnBlue.Enabled = false;
+            }
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            if (lblColor.Text == "RED")
+            {
+                lblScoreCounter.Text = (Convert.ToInt16(lblScoreCounter.Text) + 1).ToString();
+                SelectColor();
+            }
+            else
+            {
+                SelectColor();
+            }
+        }
+
+        private void btnWhite_Click(object sender, EventArgs e)
+        {
+            if (lblColor.Text == "WHITE")
+            {
+                lblScoreCounter.Text = (Convert.ToInt16(lblScoreCounter.Text) + 1).ToString();
+                SelectColor();
+            }
+            else
+            {
+                SelectColor();
+            }
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            if (lblColor.Text == "GREEN")
+            {
+                lblScoreCounter.Text = (Convert.ToInt16(lblScoreCounter.Text) + 1).ToString();
+                SelectColor();
+            }
+            else
+            {
+                SelectColor();
+            }
+        }
+
+        private void btnPurple_Click(object sender, EventArgs e)
+        {
+            if (lblColor.Text == "PURPLE")
+            {
+                lblScoreCounter.Text = (Convert.ToInt16(lblScoreCounter.Text) + 1).ToString();
+                SelectColor();
+            }
+            else
+            {
+                SelectColor();
+            }
+        }
+
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            if (lblColor.Text == "GRAY")
+            {
+                lblScoreCounter.Text = (Convert.ToInt16(lblScoreCounter.Text) + 1).ToString();
+                SelectColor();
+            }
+            else
+            {
+                SelectColor();
+            }
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            if (lblColor.Text == "YELLOW")
+            {
+                lblScoreCounter.Text = (Convert.ToInt16(lblScoreCounter.Text) + 1).ToString();
+                SelectColor();
+            }
+            else
+            {
+                SelectColor();
+            }
+        }
+
+        private void btnBlack_Click(object sender, EventArgs e)
+        {
+            if (lblColor.Text == "BLACK")
+            {
+                lblScoreCounter.Text = (Convert.ToInt16(lblScoreCounter.Text) + 1).ToString();
+                SelectColor();
+            }
+            else
+            {
+                SelectColor();
+            }
+        }
+
+        private void btnOrange_Click(object sender, EventArgs e)
+        {
+            if (lblColor.Text == "ORANGE")
+            {
+                lblScoreCounter.Text = (Convert.ToInt16(lblScoreCounter.Text) + 1).ToString();
+                SelectColor();
+            }
+            else
+            {
+                SelectColor();
+            }
+        }
+
+        private void btnPink_Click(object sender, EventArgs e)
+        {
+            if (lblColor.Text == "PINK")
+            {
+                lblScoreCounter.Text = (Convert.ToInt16(lblScoreCounter.Text) + 1).ToString();
+                SelectColor();
+            }
+            else
+            {
+                SelectColor();
+            }
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            if (lblColor.Text == "BLUE")
+            {
+                lblScoreCounter.Text = (Convert.ToInt16(lblScoreCounter.Text) + 1).ToString();
+                SelectColor();
+            }
+            else
+            {
+                SelectColor();
+            }
+        }
     }
 }
